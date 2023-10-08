6ed5fd656e7c086d1c69086a3661907e3a3fa928
Kareem Waly <48871160+kareemkhaled98@users.noreply.github.com>
2023-10-08 12:01:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-// JavaScript for smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    // Handle smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('.navigation a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
     });
+
+    // Function to scroll smoothly to a target element
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust the offset as needed
+                behavior: 'smooth',
+            });
+        }
+    }
+
+    // Add additional JavaScript functionality for your sections and elements here
+
+    // Example: Toggle a class on button click
+    const toggleButton = document.querySelector('.toggle-button');
+    const toggleElement = document.querySelector('.toggle-element');
+    
+    toggleButton.addEventListener('click', function () {
+        toggleElement.classList.toggle('active');
+    });
+
+    // Example: Form submission handling
+    const contactForm = document.querySelector('#contact-form');
+    if (contactForm) {
+        contactForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            // Handle form submission logic here
+        });
+    }
+
+    // Add more JavaScript functionality as needed for your specific requirements
 });
