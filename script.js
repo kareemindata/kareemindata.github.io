1e9ab9d421b05f70f159c1baf55ae9a613ebf3ae
Kareem Waly <48871160+kareemkhaled98@users.noreply.github.com>
2023-10-09 15:03:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -42,23 +42,41 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('dark-mode');
     });
 
-    // Rest of your JavaScript code here
-
-    // Example: Handling Form Submission
+    // Handling Form Submission
     const contactForm = document.querySelector('#contact-form');
     if (contactForm) {
         contactForm.addEventListener('submit', function (e) {
             e.preventDefault();
             const formData = new FormData(contactForm);
+            
             // You can now handle form data, for example, by sending it to a server using the Fetch API.
             // Replace the following lines with your specific form submission logic:
-            console.log('Form submitted with the following data:');
-            for (let [name, value] of formData) {
-                console.log(`${name}: ${value}`);
-            }
+            
+            // Example: Sending form data to a server using Fetch API
+            fetch('https://your-api-endpoint.com', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response from the server
+                console.log('Form submission successful:', data);
+                // You can add code here to show a success message or redirect the user.
+            })
+            .catch(error => {
+                console.error('Error submitting the form:', error);
+                // Handle the error, e.g., display an error message to the user.
+            });
         });
     }
 
     // Add more JavaScript functionality as needed for your specific requirements
 
+    // Example: Adding an event listener to another element
+    const exampleElement = document.getElementById('example-element');
+    if (exampleElement) {
+        exampleElement.addEventListener('click', function () {
+            // Your code here
+        });
+    }
 });
